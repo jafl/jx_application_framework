01649e9c1e7a373b716d38952cad39979faa177f
John Lindal <github@newplanetsoftware.com>
2012-11-05 14:49:30 -0800

    add back in missing Code Medic file

---
+++ programs/code_medic/code/CMStylerStubs.cpp
@@ -0,0 +1,137 @@
+/******************************************************************************
+ CMStylerStubs.cpp
+
+	Copyright © 2001 by John Lindal. All rights reserved.
+
+ ******************************************************************************/
+
+#include <cmStdInc.h>
+#include "CMStylerStubs.h"
+
+/******************************************************************************
+ CBBourneShellStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBBourneShellStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBBourneShellStyler::Shutdown()
+{
+}
+
+/******************************************************************************
+ CBCSharpStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBCSharpStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBCSharpStyler::Shutdown()
+{
+}
+
+/******************************************************************************
+ CBCShellStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBCShellStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBCShellStyler::Shutdown()
+{
+}
+
+/******************************************************************************
+ CBEiffelStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBEiffelStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBEiffelStyler::Shutdown()
+{
+}
+
+/******************************************************************************
+ CBEiffelStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBJavaScriptStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBJavaScriptStyler::Shutdown()
+{
+}
+
+/******************************************************************************
+ CBPerlStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBPerlStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBPerlStyler::Shutdown()
+{
+}
+
+/******************************************************************************
+ CBPythonStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBPythonStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBPythonStyler::Shutdown()
+{
+}
+
+/******************************************************************************
+ CBTCLStyler
+
+ ******************************************************************************/
+
+CBStylerBase*
+CBTCLStyler::Instance()
+{
+	return NULL;
+}
+
+void
+CBTCLStyler::Shutdown()
+{
+}
---
+++ programs/code_medic/code/CMStylerStubs.h
@@ -0,0 +1,81 @@
+/******************************************************************************
+ CMStylerStubs.h
+
+	Copyright © 2001 by John Lindal. All rights reserved.
+
+ ******************************************************************************/
+
+#ifndef _H_CMStylerStubs
+#define _H_CMStylerStubs
+
+#if !defined _J_UNIX && !defined ACE_LACKS_PRAGMA_ONCE
+#pragma once
+#endif
+
+#include "CBStylerBase.h"
+
+class CBBourneShellStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+class CBCSharpStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+class CBCShellStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+class CBEiffelStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+class CBJavaScriptStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+class CBPerlStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+class CBPythonStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+class CBTCLStyler
+{
+public:
+
+	static CBStylerBase*	Instance();
+	static void				Shutdown();
+};
+
+#endif
