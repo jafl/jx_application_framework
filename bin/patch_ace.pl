72e8928318463ba0979ae2b73aed9dc35edab30b
John Lindal <github@newplanetsoftware.com>
2016-10-09 11:27:41 -0700

    add patch_ace script to fix ACE exception handling

---
+++ bin/patch_ace.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $path;
+if (!($path = $ARGV[0]))
+	{
+	print "usage: $0 path_to_source\n";
+	exit;
+	}
+
+#
+# config-g++-common.h
+#
+
+my $file = $path.'/config-g++-common.h';
+
+my $text;
+open(F, '< '.$file);
+{
+local $/;
+$text = <F>;
+}
+close(F);
+
+if ($text =~ s|\n\n#define ACE_NEW_THROWS_EXCEPTIONS\n|\n#if defined (__EXCEPTIONS)\n#define ACE_NEW_THROWS_EXCEPTIONS\n#endif /\* __EXCEPTIONS \*/\n|s;)
+	{
+	open(F, '> '.$file);
+	print F $text;
+	close(F);
+	}
