45ad593dbf1cd532ccb976c8b8f74032acb8ae35
John Lindal <github@newplanetsoftware.com>
2016-12-13 19:40:07 -0800

    create JStringMatch

---
+++ libjcore/code/JStringMatch.cpp
@@ -0,0 +1,225 @@
+/******************************************************************************
+ JStringMatch.cpp
+
+						The JString Match Class
+
+	Stores information about a successful match by a JStringIterator.  As
+	with JStringIterator, this object is automatically updated when the
+	underlying JString changes.
+
+	Copyright (C) 2016 by John Lindal. All rights reserved.
+
+ ******************************************************************************/
+
+#include <JStringMatch.h>
+#include <JString.h>
+#include <jAssert.h>
+
+/******************************************************************************
+ Constructor (protected)
+
+	If a list of submatches is provided, we take ownership of the list.
+
+	The creator must also call one if Set(First|Last)CharacterIndex
+
+ ******************************************************************************/
+
+JStringMatch::JStringMatch
+	(
+	const JString&			target,
+	const JUtf8ByteRange&	byteRange,
+	JArray<JUtf8ByteRange>*	list
+	)
+	:
+	itsTarget(target),
+	itsByteRange(byteRange),
+	itsSubmatchList(list)
+{
+	assert( !itsByteRange.IsEmpty() );		// avoid potential infinite loops
+}
+
+/******************************************************************************
+ Destructor
+
+ ******************************************************************************/
+
+JStringMatch::~JStringMatch()
+{
+	jdelete itsSubmatchList;
+}
+
+/******************************************************************************
+ Copy constructor
+
+ ******************************************************************************/
+
+JStringMatch::JStringMatch
+	(
+	const JStringMatch& source
+	)
+	:
+	itsTarget(source.itsTarget),
+	itsByteRange(source.itsByteRange),
+	itsCharacterRange(source.itsCharacterRange),
+	itsSubmatchList(NULL)
+{
+	if (source.itsSubmatchList != NULL)
+		{
+		itsSubmatchList = jnew JArray<JUtf8ByteRange>(*(source.itsSubmatchList));
+		assert( itsSubmatchList != NULL );
+		}
+}
+
+/******************************************************************************
+ SetFirstCharacterIndex (protected)
+
+ ******************************************************************************/
+
+void
+JStringMatch::SetFirstCharacterIndex
+	(
+	const JIndex index
+	)
+{
+	assert( itsCharacterRange.IsNothing() );
+	assert( index > 0 );
+	itsCharacterRange.first = index;
+	// itsCharacterRange.first will be computed when needed
+}
+
+/******************************************************************************
+ SetLastCharacterIndex (protected)
+
+ ******************************************************************************/
+
+void
+JStringMatch::SetLastCharacterIndex
+	(
+	const JIndex index
+	)
+{
+	assert( itsCharacterRange.IsNothing() );
+	assert( index > 0 );
+
+	const JSize count = JString::CountCharacters(itsTarget.GetBytes(), itsByteRange);
+	assert( count <= index );
+
+	itsCharacterRange.Set(index - count + 1, index);
+}
+
+/******************************************************************************
+ GetCharacterCount
+
+ ******************************************************************************/
+
+JSize
+JStringMatch::GetCharacterCount()
+	const
+{
+	FinishCharacterRange();
+	return itsCharacterRange.GetCount();
+}
+
+/******************************************************************************
+ GetCharacterRange
+
+ ******************************************************************************/
+
+JCharacterRange
+JStringMatch::GetCharacterRange
+	(
+	const JIndex submatchIndex
+	)
+	const
+{
+	if (submatchIndex == 0)
+		{
+		FinishCharacterRange();
+		return itsCharacterRange;
+		}
+	else if (itsSubmatchList != NULL && itsSubmatchList->IndexValid(submatchIndex))
+		{
+		const JUtf8ByteRange ur = itsSubmatchList->GetElement(submatchIndex);
+
+		JCharacterRange cr;
+		cr.SetFirstAndCount(
+			itsCharacterRange.first +
+				JString::CountCharacters(itsTarget.GetBytes() + itsByteRange.first - 1,
+										 ur.first - itsByteRange.first) - 1,
+			JString::CountCharacters(itsTarget.GetBytes() + ur.first - 1, ur.GetCount()));
+
+		return cr;
+		}
+	else
+		{
+		return JCharacterRange();
+		}
+}
+
+/******************************************************************************
+ GetString
+
+ ******************************************************************************/
+
+JString
+JStringMatch::GetString()
+	const
+{
+	return JString(itsTarget.GetBytes(), itsByteRange);
+}
+
+/******************************************************************************
+ GetSubstring
+
+ ******************************************************************************/
+
+JString
+JStringMatch::GetSubstring
+	(
+	const JIndex index
+	)
+	const
+{
+	if (itsSubmatchList != NULL && itsSubmatchList->IndexValid(index))
+		{
+		return JString(itsTarget.GetBytes(), itsSubmatchList->GetElement(index));
+		}
+	else
+		{
+		return JString();
+		}
+}
+
+/******************************************************************************
+ StringChanged (protected)
+
+ ******************************************************************************/
+
+void
+JStringMatch::StringChanged
+	(
+	const JBroadcaster::Message& message
+	)
+{
+	// TODO
+}
+
+/******************************************************************************
+ FinishCharacterRange (private)
+
+	Updating itsCharacterRange.last does not change conceptual constness,
+	because this is just an optimization.
+
+ ******************************************************************************/
+
+void
+JStringMatch::FinishCharacterRange()
+	const
+{
+	if (itsCharacterRange.IsEmpty())
+		{
+		const_cast<JCharacterRange*>(&itsCharacterRange)->last =
+			itsCharacterRange.first +
+			JString::CountCharacters(itsTarget.GetBytes(), itsByteRange) - 1;
+		}
+}
---
+++ libjcore/code/JStringMatch.h
@@ -0,0 +1,105 @@
+/******************************************************************************
+ JStringMatch.h
+
+	Copyright (C) 2016 by John Lindal. All rights reserved.
+
+ ******************************************************************************/
+
+#ifndef _H_JStringMatch
+#define _H_JStringMatch
+
+#include <JString.h>
+#include <JArray.h>
+
+class JStringIterator;
+
+class JStringMatch
+{
+	friend class JString;			// to modify string
+	friend class JStringIterator;	// construction
+	friend class JRegex;			// construction
+
+public:
+
+	JStringMatch(const JStringMatch& source);
+
+	~JStringMatch();
+
+	JSize	GetCharacterCount() const;
+	JSize	GetByteCount() const;
+	JSize	GetSubstringCount() const;
+
+	JString	GetString() const;
+	JString	GetSubstring(const JIndex submatchIndex) const;
+
+	JCharacterRange	GetCharacterRange(const JIndex submatchIndex = 0) const;
+	JUtf8ByteRange	GetUtf8ByteRange(const JIndex submatchIndex = 0) const;
+
+protected:
+
+	JStringMatch(const JString& target, const JUtf8ByteRange& byteRange,
+				 JArray<JUtf8ByteRange>* list = NULL);
+
+	void	SetFirstCharacterIndex(const JIndex index);
+	void	SetLastCharacterIndex(const JIndex index);
+	void	StringChanged(const JBroadcaster::Message& message);
+
+private:
+
+	const JString&			itsTarget;
+	JUtf8ByteRange			itsByteRange;
+	JCharacterRange			itsCharacterRange;	// empty if have not computed # of characters
+	JArray<JUtf8ByteRange>*	itsSubmatchList;	// can be NULL
+
+private:
+
+	void	FinishCharacterRange() const;
+
+	// not allowed
+
+	const JStringMatch& operator=(const JStringMatch& source);
+};
+
+
+/******************************************************************************
+ GetByteCount
+
+ ******************************************************************************/
+
+inline JSize
+JStringMatch::GetByteCount()
+	const
+{
+	return itsByteRange.GetCount();
+}
+
+/******************************************************************************
+ GetSubstringCount
+
+ ******************************************************************************/
+
+inline JSize
+JStringMatch::GetSubstringCount()
+	const
+{
+	return (itsSubmatchList == NULL ? 0 : itsSubmatchList->GetElementCount());
+}
+
+/******************************************************************************
+ GetUtf8ByteRange
+
+ ******************************************************************************/
+
+inline JUtf8ByteRange
+JStringMatch::GetUtf8ByteRange
+	(
+	const JIndex submatchIndex
+	)
+	const
+{
+	return (submatchIndex == 0 ? itsByteRange :
+			itsSubmatchList == NULL || !itsSubmatchList->IndexValid(submatchIndex) ? JUtf8ByteRange() :
+			itsSubmatchList->GetElement(submatchIndex));
+}
+
+#endif
