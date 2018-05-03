3c4f51eb12cf65134d096a7c300abb0e2b4cf35b
John Lindal <github@newplanetsoftware.com>
2018-05-02 20:27:24 -0700

    fix FTC window resize logic

--- libjx/code/JXWindow.cpp
+++ libjx/code/JXWindow.cpp
@@ -1800,8 +1800,8 @@ JXWindow::FTCAdjustSize
 	const JCoordinate origW = itsBounds.width(),
 					  origH = itsBounds.height();
 
-	JCoordinate w = origW + dw,
-				h = origH + dh;
+	JCoordinate w = origW,
+				h = origH;
 
 	if (itsHasMinSizeFlag)
 		{
