14d9a3101e06bcdd05d10f82702d04c64716aaa4
John Lindal <github@newplanetsoftware.com>
2018-06-08 18:14:39 -0500

    add more c++ keywords

--- programs/code_crusader/code/CBCCompleter.cpp
+++ programs/code_crusader/code/CBCCompleter.cpp
@@ -25,12 +25,14 @@ static const JCharacter* kKeywordList[] =
 
 	// C++
 
-	"and", "and_eq", "asm", "bitand", "bitor", "bool", "catch", "class", "compl",
-	"const_cast", "delete", "dynamic_cast", "explicit", "export", "false",
-	"friend", "inline", "mutable", "namespace", "new", "not", "not_eq",
-	"operator", "or", "or_eq", "overload", "private", "protected", "public",
-	"reinterpret_cast", "static_cast", "template", "this", "throw", "true",
-	"try", "typeid", "typename", "using", "virtual", "wchar_t", "xor", "xor_eq",
+	"and", "and_eq", "asm", "bitand", "bitor", "bool", "catch", "class",
+	"compl", "const_cast", "delete", "dynamic_cast", "explicit", "export",
+	"false", "final", "friend", "inline", "mutable", "namespace", "new",
+	"noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq",
+	"overload", "override", "private", "protected", "public",
+	"reinterpret_cast", "static_cast", "template", "this", "thread_local",
+	"throw", "true", "try", "typeid", "typename", "using", "virtual",
+	"wchar_t", "xor", "xor_eq",
 
 	// preprocessor
 
