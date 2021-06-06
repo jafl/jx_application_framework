553ea5af0d9eec79411d64ecd47e7f10d9bc4a65
John Lindal <github@newplanetsoftware.com>
2021-06-05 20:06:42 -0700

    jcc: D completer

---
+++ programs/code_crusader/code/CBDCompleter.cpp
@@ -0,0 +1,130 @@
+/******************************************************************************
+ CBDCompleter.cpp
+
+	BASE CLASS = CBStringCompleter
+
+	Copyright (C) 2021 by John Lindal.
+
+ ******************************************************************************/
+
+#include "CBDCompleter.h"
+#include <jAssert.h>
+
+CBDCompleter* CBDCompleter::itsSelf = nullptr;
+
+static const JUtf8Byte* kKeywordList[] =
+{
+	"abstract", "alias", "align", "asm", "assert", "auto", "body", "bool",
+	"break", "byte", "case", "cast", "catch", "cdouble", "cent", "cfloat",
+	"char", "class", "const", "continue", "creal", "dchar", "debug",
+	"default", "delegate", "delete", "deprecated", "do", "double", "else",
+	"enum", "export", "extern", "false", "final", "finally", "float",
+	"for", "foreach", "foreach_reverse", "function", "goto", "idouble",
+	"if", "ifloat", "immutable", "import", "in", "inout", "int",
+	"interface", "invariant", "ireal", "is", "lazy", "long", "macro",
+	"mixin", "module", "new", "nothrow", "null", "out", "override",
+	"package", "pragma", "private", "protected", "public", "pure", "real",
+	"ref", "return", "scope", "shared", "short", "static", "struct", "super",
+	"switch", "synchronized", "template", "this", "throw", "true", "try",
+	"typeid", "typeof", "ubyte", "ucent", "uint", "ulong", "union",
+	"unittest", "ushort", "version", "void", "wchar", "while", "with",
+
+	"__DATE__", "__EOF__", "__FILE__", "__FILE_FULL_PATH__", "__FUNCTION__",
+	"__LINE__", "__MODULE__", "__PRETTY_FUNCTION__", "__TIME__",
+	"__TIMESTAMP__", "__VENDOR__", "__VERSION__",
+
+	"__gshared", "__traits", "__vector", "__parameters"
+};
+
+const JSize kKeywordCount = sizeof(kKeywordList)/sizeof(JUtf8Byte*);
+
+/******************************************************************************
+ Instance (static)
+
+ ******************************************************************************/
+
+static JBoolean recursiveInstance = kJFalse;
+
+CBStringCompleter*
+CBDCompleter::Instance()
+{
+	if (itsSelf == nullptr && !recursiveInstance)
+		{
+		recursiveInstance = kJTrue;
+
+		itsSelf = jnew CBDCompleter;
+		assert( itsSelf != nullptr );
+
+		recursiveInstance = kJFalse;
+		}
+
+	return itsSelf;
+}
+
+/******************************************************************************
+ Shutdown (static)
+
+ ******************************************************************************/
+
+void
+CBDCompleter::Shutdown()
+{
+	jdelete itsSelf;
+}
+
+/******************************************************************************
+ Constructor (protected)
+
+ ******************************************************************************/
+
+CBDCompleter::CBDCompleter()
+	:
+	CBStringCompleter(kCBDLang, kKeywordCount, kKeywordList, kJTrue)
+{
+}
+
+/******************************************************************************
+ Destructor
+
+ ******************************************************************************/
+
+CBDCompleter::~CBDCompleter()
+{
+	itsSelf = nullptr;
+}
+
+/******************************************************************************
+ IsWordCharacter (virtual protected)
+
+	We include the namespace operator (.) to allow completion of fully
+	qualified names.
+
+ ******************************************************************************/
+
+JBoolean
+CBDCompleter::IsWordCharacter
+	(
+	const JUtf8Character&	c,
+	const JBoolean			includeNS
+	)
+	const
+{
+	return JI2B(c.IsAlnum() || c == '_' || (includeNS && c == '.'));
+}
+
+/******************************************************************************
+ GetDefaultWordList (static)
+
+	Must be static because may be called before object is created.
+
+ ******************************************************************************/
+
+JSize
+CBDCompleter::GetDefaultWordList
+	(
+	const JUtf8Byte*** list
+	)
+{
+	*list = kKeywordList;
+	return kKeywordCount;
+}
---
+++ programs/code_crusader/code/CBDCompleter.h
@@ -0,0 +1,43 @@
+/******************************************************************************
+ CBDCompleter.h
+
+	Copyright (C) 2021 by John Lindal.
+
+ ******************************************************************************/
+
+#ifndef _H_CBDCompleter
+#define _H_CBDCompleter
+
+#include "CBStringCompleter.h"
+
+class CBDCompleter : public CBStringCompleter
+{
+public:
+
+	static CBStringCompleter*	Instance();
+	static void					Shutdown();
+
+	virtual ~CBDCompleter();
+
+	static JSize	GetDefaultWordList(const JUtf8Byte*** list);
+
+protected:
+
+	CBDCompleter();
+
+	virtual JBoolean	IsWordCharacter(const JUtf8Character& c,
+										const JBoolean includeNS) const override;
+
+private:
+
+	static CBDCompleter*	itsSelf;
+
+private:
+
+	// not allowed
+
+	CBDCompleter(const CBDCompleter& source);
+	const CBDCompleter& operator=(const CBDCompleter& source);
+};
+
+#endif
